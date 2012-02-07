7668fea3231ac2e64e100ca759c767eee8c53813
Josh Vote <Josh.Vote@csiro.au>
2012-02-07 06:27:21 +0000

    Backend support for known layers. CSWRecord selection logic is now handled at the backend instead of through the javascript.

--- src/main/java/org/auscope/portal/server/web/controllers/KnownLayerController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/KnownLayerController.java
@@ -3,10 +3,14 @@ package org.auscope.portal.server.web.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.auscope.portal.server.web.AbstractKnownLayer;
+import org.auscope.portal.csw.record.CSWRecord;
+import org.auscope.portal.server.domain.auscope.KnownLayerAndRecords;
+import org.auscope.portal.server.domain.auscope.KnownLayerGrouping;
+import org.auscope.portal.server.web.service.KnownLayerService;
+import org.auscope.portal.server.web.view.JSONModelAndView;
+import org.auscope.portal.server.web.view.ViewCSWRecordFactory;
 import org.auscope.portal.server.web.view.ViewKnownLayerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +18,48 @@ import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Contains methods for requesting the list of known feature types
- * @author vot002
+ * @author Josh Vote
  *
  */
 @Controller
-public class KnownLayerController extends BasePortalController {
+public class KnownLayerController extends BaseCSWController {
 
-    private List knownTypes;
-    private ViewKnownLayerFactory viewFactory;
+    /** Used for requesting groupings of CSWRecords under known layers*/
+    private KnownLayerService knownLayerService;
+    /** Used for converting data to something the view can understand*/
+    private ViewKnownLayerFactory viewKnownLayerFactory;
 
     @Autowired
-    public KnownLayerController(@Qualifier("knownTypes") ArrayList knownTypes,
-            ViewKnownLayerFactory viewFactory) {
-        this.knownTypes = knownTypes;
-        this.viewFactory = viewFactory;
+    public KnownLayerController(KnownLayerService knownLayerService,
+            ViewKnownLayerFactory viewFactory, ViewCSWRecordFactory viewCSWRecordFactory) {
+        super(viewCSWRecordFactory);
+        this.knownLayerService = knownLayerService;
+        this.viewKnownLayerFactory = viewFactory;
+    }
 
+    private ModelAndView generateResponse(List<KnownLayerAndRecords> knownLayers, List<CSWRecord> unmappedRecords) {
+        List<ModelMap> viewUnmappedRecords = new ArrayList<ModelMap>();
+        for (CSWRecord rec : unmappedRecords) {
+            viewUnmappedRecords.add(viewCSWRecordFactory.toView(rec));
+        }
+
+        List<ModelMap> viewKnownLayers = new ArrayList<ModelMap>();
+        for (KnownLayerAndRecords kl : knownLayers) {
+            ModelMap viewKnownLayer = viewKnownLayerFactory.toView(kl.getKnownLayer());
+
+            List<ModelMap> viewMappedRecords = new ArrayList<ModelMap>();
+            for (CSWRecord rec : kl.getBelongingRecords()) {
+                viewMappedRecords.add(viewCSWRecordFactory.toView(rec));
+            }
+
+            viewKnownLayer.put("records", viewMappedRecords);
+            viewKnownLayers.add(viewKnownLayer);
+        }
+
+        //Generate our response (and augment it with a list of unmapped records)
+        ModelMap responseModel = generateResponseModel(true, viewKnownLayers, null, "", null);
+        responseModel.put("unmappedRecords", viewUnmappedRecords);
+        return new JSONModelAndView(responseModel);
     }
 
     /**
@@ -39,11 +70,8 @@ public class KnownLayerController extends BasePortalController {
      */
     @RequestMapping("getKnownLayers.do")
     public ModelAndView getKnownLayers() {
-        List<ModelMap> viewRepresentations = new ArrayList<ModelMap>();
-        for (Object k : knownTypes) {
-            viewRepresentations.add(viewFactory.toView((AbstractKnownLayer) k));
-        }
+        KnownLayerGrouping grouping = knownLayerService.groupKnownLayerRecords();
 
-        return generateJSONResponseMAV(true, viewRepresentations, "");
+        return generateResponse(grouping.getKnownLayers(), grouping.getUnmappedRecords());
     }
 }
