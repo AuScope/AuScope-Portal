3cbc0155707265d31fb1c48aec9c2ca1f8b95880
Adam Brown <Adam.Brown@csiro.au>
2012-11-21 03:07:49 +0000

    Updating UI to use ext JS graphs.

--- src/main/java/org/auscope/portal/mscl/MSCLWFSService.java
+++ src/main/java/org/auscope/portal/mscl/MSCLWFSService.java
@@ -1,22 +1,40 @@
 package org.auscope.portal.mscl;
 
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
 import java.net.ConnectException;
+import java.net.URLEncoder;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.httpclient.ConnectTimeoutException;
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpMethodBase;
 import org.auscope.portal.core.server.http.HttpServiceCaller;
 import org.auscope.portal.core.services.BaseWFSService;
 import org.auscope.portal.core.services.methodmakers.WFSGetFeatureMethodMaker;
 import org.auscope.portal.core.services.methodmakers.WFSGetFeatureMethodMaker.ResultType;
+import org.auscope.portal.core.services.namespaces.IterableNamespace;
+import org.opengis.feature.Feature;
+import org.opengis.feature.Property;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.PropertyIsBetween;
+import org.opengis.filter.PropertyIsEqualTo;
+import org.opengis.filter.spatial.Intersects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.vividsolutions.jts.geom.Envelope;
 
 /**
  * This class is a specialisation of BaseWFSService. Its purpose is to allow access to MSCL's
  * observations WFS. It circumvents an issue with the BaseWFSService's generateWFSRequest(...) 
  * implementation by allowing a null or blank SRS. The other implementation converted such SRSs
- * to a default value which causes a reprojection to occur later on which would always fail.
+ * to a default value which causes a reprojection to occur later on which it would always fail.
  * @author bro879
  */
 @Service
@@ -65,6 +83,46 @@ public class MSCLWFSService extends BaseWFSService {
 		return httpServiceCaller.getMethodResponseAsString(method);
 	}
 	
+	public String getObservations(
+	        final String serviceUrl,
+	        final String boreholeName,
+	        final String startDepth,
+	        final String endDepth) throws ConnectException, ConnectTimeoutException, UnknownHostException, Exception {
+	    // .../wfs?request=GetFeature&typename=mscl:scanned_data&filter=%3CFilter%3E%0D%0A%09%3CPropertyIs%3E%0D%0A%09%09%3CPropertyName%3Emscl%3Aborehole%3C%2FPropertyName%3E%0D%0A%09%09%3CLiteral%3EPRC-5%3C%2FLiteral%3E%0D%0A%09%3C%2FPropertyIs%3E%0D%0A%09%3CPropertyIsBetween%3E%0D%0A%09%09%3CPropertyName%3Emscl%3Adepth%3C%2FPropertyName%3E%0D%0A%09%09%3CLowerBoundary%3E%0D%0A%09%09%09%3CLiteral%3E66.9%3C%2FLiteral%3E%0D%0A%09%09%3C%2FLowerBoundary%3E%0D%0A%09%09%3CUpperBoundary%3E%0D%0A%09%09%09%3CLiteral%3E89%3C%2FLiteral%3E%0D%0A%09%09%3C%2FUpperBoundary%3E%0D%0A%09%3C%2FPropertyIsBetween%3E%0D%0A%3C%2FFilter%3E
+
+        String filterString = String.format( 
+            "<Filter>" +
+            "   <PropertyIs>" +
+            "       <PropertyName>mscl:borehole</PropertyName>" +
+            "       <Literal>%s</Literal>" +
+            "   </PropertyIs>" +
+            "   <PropertyIsBetween>" +
+            "       <PropertyName>mscl:depth</PropertyName>" +
+            "       <LowerBoundary>" +
+            "           <Literal>%s</Literal>" +
+            "       </LowerBoundary>" +
+            "       <UpperBoundary>" +
+            "           <Literal>%s</Literal>" +
+            "       </UpperBoundary>" +
+            "   </PropertyIsBetween>" +
+            "</Filter>", 
+            boreholeName,
+            startDepth,
+            endDepth);
+
+	    HttpMethodBase method = this.generateWFSRequest(
+	        serviceUrl,
+            "mscl:scanned_data", // TODO: hard-coding feels bad but this is, after all, an MSCL-specific service...
+            null,
+            filterString,
+            null,
+            null,
+            WFSGetFeatureMethodMaker.ResultType.Results,
+            "");
+	    
+	    return httpServiceCaller.getMethodResponseAsString(method);
+	}
+	
 	/**
 	 * Generates a WFS request based on the arguments provided.
 	 * @param wfsUrl
@@ -88,15 +146,14 @@ public class MSCLWFSService extends BaseWFSService {
 	 */
 	@Override
 	protected HttpMethodBase generateWFSRequest(
-			String wfsUrl, 
+			String wfsUrl,
 			String featureType, 
-			String featureId, 
-			String filterString, 
-			Integer maxFeatures, 
-			String srs, 
+			String featureId,
+			String filterString,
+			Integer maxFeatures,
+			String srs,
 			ResultType resultType,
 			String outputFormat) {
-
 		int max = maxFeatures == null ? 0 : maxFeatures.intValue();
 
         if (featureId == null) {
--- src/main/webapp/js/auscope/layer/querier/wfs/knownlayerfactories/MSCLFactory.js
+++ src/main/webapp/js/auscope/layer/querier/wfs/knownlayerfactories/MSCLFactory.js
@@ -1,6 +1,9 @@
 /**
  * A factory for creating an MSCL presentation pane.
  */
+Ext.require('Ext.chart.*');
+Ext.require(['Ext.Window', 'Ext.fx.target.Sprite', 'Ext.layout.container.Fit', 'Ext.window.MessageBox']);
+
 Ext.define('auscope.layer.querier.wfs.knownlayerfactories.MSCLFactory', {
     extend : 'portal.layer.querier.wfs.knownlayerfactories.BaseFactory',
 
@@ -19,59 +22,267 @@ Ext.define('auscope.layer.querier.wfs.knownlayerfactories.MSCLFactory', {
      * Overrides abstract parseKnownLayerFeature
      */
     parseKnownLayerFeature : function(featureId, parentKnownLayer, parentOnlineResource) {
-    	// This is of the format: http://$domain:$port/$some_path/wfs
+    	// Define the model:
+		Ext.define('DiameterPoint', {
+			extend: 'Ext.data.Model',
+			fields: ['depth', 'diameter']
+		});
+
+		// This is of the format: http://$domain:$port/$some_path/wfs
     	var wfsUrl = parentOnlineResource.get('url');
-    	
+
+		// Create the datastore:
+		var store = Ext.create('Ext.data.Store', {
+			model : 'DiameterPoint',
+			proxy : {
+			    type : 'ajax',
+			    url : 'getMsclObservationsForGraph.do',
+			    pageParam: undefined,
+			    startParam: undefined,
+			    limitParam: undefined,
+    			extraParams : {
+    				// .../wfs?request=GetFeature&typename=mscl:scanned_data&filter=%3CFilter%3E%0D%0A%09%3CPropertyIs%3E%0D%0A%09%09%3CPropertyName%3Emscl%3Aborehole%3C%2FPropertyName%3E%0D%0A%09%09%3CLiteral%3EPRC-5%3C%2FLiteral%3E%0D%0A%09%3C%2FPropertyIs%3E%0D%0A%09%3CPropertyIsBetween%3E%0D%0A%09%09%3CPropertyName%3Emscl%3Adepth%3C%2FPropertyName%3E%0D%0A%09%09%3CLowerBoundary%3E%0D%0A%09%09%09%3CLiteral%3E66.9%3C%2FLiteral%3E%0D%0A%09%09%3C%2FLowerBoundary%3E%0D%0A%09%09%3CUpperBoundary%3E%0D%0A%09%09%09%3CLiteral%3E89%3C%2FLiteral%3E%0D%0A%09%09%3C%2FUpperBoundary%3E%0D%0A%09%3C%2FPropertyIsBetween%3E%0D%0A%3C%2FFilter%3E
+    				serviceUrl : wfsUrl,
+    				boreholeName : 'PRC-5',
+    				startDepth : 66,
+    				endDepth : 200,
+    				observationsToReturn : 'ct,den1'
+    			},
+			    reader : {
+			        type : 'json',
+			        root : 'data.series'
+			    },
+				noCache: false
+			},
+			autoLoad : false
+		});
+
     	return Ext.create('portal.layer.querier.BaseComponent', {
             border : false,
             tabTitle : 'MSCL Data',
             items : [{
-            	itemId : 'msclDataPanel',
-                xtype : 'fieldset',
-                title : 'Petrophysical Observations'
-            }],
-            listeners : {
-            	render : function(panel, eOpts) {
-            		Ext.Ajax.request({
-                		url : 'getMsclObservations.do',
-                		params : {
-                			serviceUrl : wfsUrl,
-                			typeName : 'sa:SamplingFeatureCollection',
-                			featureId : 'sampling_feature_collection.borehole_header.' + featureId
-                		},
-                		success : function(response) {
-                			var jsonResponse = Ext.JSON.decode(response.responseText);
-                			var xmlDocument = portal.util.xml.SimpleDOM.parseStringToDOM(jsonResponse.data.gml);
-                			var samplingFeatureCollection = portal.util.xml.SimpleXPath.evaluateXPathNodeArray(
-                	    			xmlDocument.documentElement,
-                	    			'gml:featureMembers/sa:SamplingFeatureCollection')[0];
-                			var simpleFactory = Ext.create('portal.layer.querier.wfs.factories.SimpleFactory', {});
-                			var xmlTreeComponentWithDownloadButton = simpleFactory.parseNode(samplingFeatureCollection, wfsUrl, { height : 250 });
-                			var gmlId = portal.util.xml.SimpleXPath.evaluateXPathString(samplingFeatureCollection, '@gml:id');
-                			
-                			// TODO: This isn't good. I'm modifying the behaviour of the button that simpleFactory has 
-                			// added to the XML Tree view. The modification is to change its target from requestFeature.do to
-                			// getMsclObservations.do which works-around an assumption made by requestFeature.do that it can
-                			// assign a default SRS where one isn't present.
-                			// Since I'm expecting the behaviour and the presentation of this SA data to be completely changed
-                			// anyway I'm just using this heavy-handed and unglamourous approach as a temporary measure.
-                			var button = xmlTreeComponentWithDownloadButton.getDockedItems()[0].items.items[0];
-                			button.handler = function() {
-                                portal.util.FileDownloader.downloadFile('downloadGMLAsZip.do', {
-                                    serviceUrls : 
-                                    	portal.util.URL.base + 
-                                    	"getMsclObservations.do" +
-                        				"?serviceUrl=" + wfsUrl + 
-                        				"&typeName=sa:SamplingFeatureCollection" +
-                        				"&featureId=" + gmlId
-                                });
-                			};
-                			panel.getComponent('msclDataPanel').add(xmlTreeComponentWithDownloadButton);
-                			panel.doLayout();
-                		}
-                	});
-            	}
-            }
-    	});
+	        	itemId : 'msclDataPanel',
+	            xtype : 'fieldset',
+	            title : 'Petrophysical Observations',
+	            items: [{
+	            	xtype : 'button',
+	            	text : 'Submit',
+	            	listeners : {
+		            	click : function (e, eOpts) {
+		            		store.load();
+		            		
+		            		Ext.create('Ext.Window', {
+		                		border      : true,
+		                        layout      : 'fit',
+		                        resizable   : false,
+		                        modal       : true,
+		                        plain       : false,
+		                        title       : 'Title', // TODO: Title
+		                        height      : 800,
+		                        width       : 400,
+		                        items		: [
+		            				Ext.create('Ext.chart.Chart', {
+		            					width: 150,
+		            					height: 1000,
+		            					shadow: false,
+		            					store: store,
+		            					axes: [
+		            						{
+		            							title: 'Depth (m)',
+		            							type: 'Numeric',
+		            							position: 'left',
+		            							fields: ['depth'],
+		            							minimum: 60,
+		            							maximum: 200,
+		            							majorTickSteps: 10,
+		            							minorTickSteps: 1
+		            						},
+		            						{
+		            							title: 'Diameter (cm)',
+		            							type: 'Numeric',
+		            							position: 'top',
+		            							fields: ['diameter'],
+		            							minimum: 4,
+		            							maximum: 7,
+		            							majorTickSteps: 5,
+		            							minorTickSteps: 1
+		            						}
+		            					],
+		            					series: [
+		            						{
+		            							type: 'line',
+		            							xField: 'diameter',
+		            							yField: 'depth',
+		            							showMarkers: false,
+		            							style: {
+		            								'stroke-width': 2
+		            							}
+		            						}
+		            					]
+		            				})
+		            			]
+		                    }).show();
+		        		}
+	            	}
+	           	}]
+	    	}]
+    	});	
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* THIS IS WORKING 
+
+
+
+
+
+
+
+
+parseKnownLayerFeature : function(featureId, parentKnownLayer, parentOnlineResource) {
+	var tp = Ext.create('Ext.TabPanel', {
+        xtype           : 'tabpanel',
+        activeItem      : 0,
+        enableTabScroll : true,
+        buttonAlign     : 'center',
+        items           : []
+    });
+
+	var win = Ext.create('Ext.Window', {
+		border      : true,
+        layout      : 'fit',
+        resizable   : false,
+        modal       : true,
+        plain       : false,
+        title       : 'Title', // TODO: Title
+        height      : 800,
+        width       : 400,
+        items		: [ tp ]
+    });
+	
+	// This is of the format: http://$domain:$port/$some_path/wfs
+	var wfsUrl = parentOnlineResource.get('url');
+	
+	// For now, just get a button that sends a hard-coded request:
+	return Ext.create('portal.layer.querier.BaseComponent', {
+        border : false,
+        tabTitle : 'MSCL Data',
+        items : [{
+        	itemId : 'msclDataPanel',
+            xtype : 'fieldset',
+            title : 'Petrophysical Observations',
+            items: [{
+            	xtype : 'button',
+            	text : 'Submit',
+            	listeners : {
+	            	click : function (e, eOpts) {
+	            		Ext.Ajax.request({
+		        			url : 'getMsclObservationsForGraph.do',
+		        			params : {
+		        				// .../wfs?request=GetFeature&typename=mscl:scanned_data&filter=%3CFilter%3E%0D%0A%09%3CPropertyIs%3E%0D%0A%09%09%3CPropertyName%3Emscl%3Aborehole%3C%2FPropertyName%3E%0D%0A%09%09%3CLiteral%3EPRC-5%3C%2FLiteral%3E%0D%0A%09%3C%2FPropertyIs%3E%0D%0A%09%3CPropertyIsBetween%3E%0D%0A%09%09%3CPropertyName%3Emscl%3Adepth%3C%2FPropertyName%3E%0D%0A%09%09%3CLowerBoundary%3E%0D%0A%09%09%09%3CLiteral%3E66.9%3C%2FLiteral%3E%0D%0A%09%09%3C%2FLowerBoundary%3E%0D%0A%09%09%3CUpperBoundary%3E%0D%0A%09%09%09%3CLiteral%3E89%3C%2FLiteral%3E%0D%0A%09%09%3C%2FUpperBoundary%3E%0D%0A%09%3C%2FPropertyIsBetween%3E%0D%0A%3C%2FFilter%3E
+		        				serviceUrl : wfsUrl,
+		        				boreholeName : 'PRC-5',
+		        				startDepth : 66,
+		        				endDepth : 200,
+		        				observationsToReturn : 'ct,den1'
+		        			},
+		        			success : function(response) {
+		        				// Define the model:
+		        				Ext.define('DiameterPoint', {
+		        					extend: 'Ext.data.Model',
+		        					fields: ['depth', 'diameter']
+		        				});
+		        
+		        				// Create the datastore:
+		        				var store = Ext.create('Ext.data.Store', {
+		        					model: 'DiameterPoint',
+		        					data: Ext.decode(response.responseText).data.series
+		        				});
+		        
+		        				chart = Ext.create('Ext.chart.Chart', {
+		        					renderTo: Ext.getBody(),
+		        					width: 150,
+		        					height: 1000,
+		        					shadow: false,
+		        					store: store,
+		        					axes: [
+		        						{
+		        							title: 'Depth (m)',
+		        							type: 'Numeric',
+		        							position: 'left',
+		        							fields: ['depth'],
+		        							minimum: 60,
+		        							maximum: 200,
+		        							majorTickSteps: 10,
+		        							minorTickSteps: 1
+		        						},
+		        						{
+		        							title: 'Diameter (cm)',
+		        							type: 'Numeric',
+		        							position: 'top',
+		        							fields: ['diameter'],
+		        							minimum: 4,
+		        							maximum: 7,
+		        							majorTickSteps: 5,
+		        							minorTickSteps: 1
+		        						}
+		        					],
+		        					series: [
+		        						{
+		        							type: 'line',
+		        							xField: 'diameter',
+		        							yField: 'depth',
+		        							showMarkers: false,
+		        							style: {
+		        								'stroke-width': 1.5
+		        							}
+		        						}
+		        					]
+		        				});
+		        				
+		        				tp.items.add(chart);
+		        				win.show();
+		        			}
+	            		});
+	            	}
+            	}
+           	}]
+    	}]
+	});	
+}
+});
+//proxy : {
+//type : 'ajax',
+//url : 'getResearchDataLayers.do',
+//reader : {
+//    type : 'json',
+//    root : 'data'
+//}
+//},
+//autoLoad : true
+*/
