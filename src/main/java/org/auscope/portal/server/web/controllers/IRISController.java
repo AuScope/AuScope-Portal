dd6f64b60ea9c80ae9e54641d2b06441013e6b5d
Adam Brown <Adam.Brown@csiro.au>
2012-12-18 03:19:42 +0000

    IRISController.java: Upgraded to use XPaths instead of extracting content manually.

--- src/main/java/org/auscope/portal/server/web/controllers/IRISController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/IRISController.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.auscope.portal.core.server.controllers.BasePortalController;
+import org.auscope.portal.core.services.namespaces.IterableNamespace;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,11 @@ import org.springframework.web.servlet.ModelAndView;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 @Controller
 public class IRISController extends BasePortalController {
@@ -37,43 +43,31 @@ public class IRISController extends BasePortalController {
             domFactory.setNamespaceAware(true); // never forget this!
             DocumentBuilder builder = domFactory.newDocumentBuilder();
             Document irisDoc = builder.parse(new ByteArrayInputStream(irisResponse.getBytes("ISO-8859-1")));
+            
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            xPath.setNamespaceContext(new IterableNamespace() {
+                {
+                    map.put("default", "http://www.data.scec.org/xml/station/");
+                }
+            });
+            
             NodeList stationEpochs = irisDoc.getDocumentElement().getElementsByTagName("StationEpoch");
-             
+            XPathExpression nameExpression = xPath.compile("default:Site/default:Name/text()");
+            XPathExpression latExpression = xPath.compile("default:Lat/text()");
+            XPathExpression lonExpression = xPath.compile("default:Lon/text()"); 
+            
             StringBuilder kml = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><name>GML Links to KML</name><description><![CDATA[GeoSciML data converted to KML]]></description>");
             
-            // For each station:
+            // For each stationEpoch:
             for (int i = 0; i < stationEpochs.getLength(); i++) {
                 Node stationEpoch = stationEpochs.item(i);
-                Node currentNode = stationEpoch.getFirstChild();
-                
-                String lat = "", lon = "", name = "";
-                
-                do {
-                    String localName = currentNode.getLocalName();
-                    if (localName == "Lat") {
-                        lat = currentNode.getTextContent().trim();
-                    } else if (localName == "Lon") {
-                        lon = currentNode.getTextContent().trim();
-                    } else if (localName == "Site") {
-                        NodeList childNodes = currentNode.getChildNodes();
-                        for (int n = 0; n < childNodes.getLength(); n++) {
-                            if (childNodes.item(n).getNodeName() == "Name") {
-                                name = currentNode.getTextContent().trim();
-                            }
-                        }
-                    }
-                    
-                    currentNode = currentNode.getNextSibling();
-                } while (currentNode != null);
-                
-                // TODO: Sort out paddle!!! Will all this work with open layers??
                 kml.append(
                         String.format(
                                 "<Placemark><name>%s</name><description><![CDATA[GENERIC_PARSER:%s]]></description><MultiGeometry><Point><Style><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png</href></Icon></IconStyle></Style><coordinates>%s,%s,0</coordinates></Point></MultiGeometry></Placemark>",
-                                name,
+                                nameExpression.evaluate(stationEpoch, XPathConstants.STRING),
                                 i,
-                                lon,
-                                lat));
+                                lonExpression.evaluate(stationEpoch, XPathConstants.STRING),
+                                latExpression.evaluate(stationEpoch, XPathConstants.STRING)));
             }
             
             kml.append("</Document></kml>");
