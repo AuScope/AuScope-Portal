9fabe7a76d0c261be3b11f680a094ec8a183e4a0
Adam Brown <Adam.Brown@csiro.au>
2012-12-18 01:15:50 +0000

    MSCLWFSService.java: Removed superfluous imports. IRISController.java: First implementation of getIRISStations(...). Needs some improvements. AuScopeRendererFactory.js: Added support for IRIS resources. jsimports.htm: Added include for IRISRenderer.js. IRISRenderer.js: Created.

--- src/main/java/org/auscope/portal/server/web/controllers/IRISController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/IRISController.java
@@ -1,15 +1,20 @@
 package org.auscope.portal.server.web.controllers;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.ByteArrayInputStream;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.auscope.portal.core.server.controllers.BasePortalController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 @Controller
 public class IRISController extends BasePortalController {
@@ -17,58 +22,65 @@ public class IRISController extends BasePortalController {
     // really suitable: the FDM is specifically for WFS whereas the
     // IRIS service doesn't use that interface, it has its own.
     // typeName is being passed in which doesn't do anything....
-    @RequestMapping("/doIRISFilter.do")
-    public ModelAndView doIRISFilter(
+    @RequestMapping("/getIRISStations.do")
+    public ModelAndView getIRISStations(
         @RequestParam("serviceUrl") String serviceUrl,
-        @RequestParam(required = false, value = "maxFeatures", defaultValue="0") int maxFeatures,
+        @RequestParam("networkCode") String networkCode,
         @RequestParam(required = false, value = "bbox") String bboxJson) {
-        
-//        StringBuilder irisResponse = new StringBuilder();
-//        try
-//        {
-//            URL irisURL = new URL("http://www.iris.edu/ws/station/query?net=S");
-//            URLConnection connection = irisURL.openConnection();
-//            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            
-//            String line;
-//            while ((line = in.readLine()) != null) { 
-//                sb.append(line);
-//            }
-//            
-//            in.close();
-//        }
-//        catch (Exception e) {
-//            return generateJSONResponseMAV(false, e, "Failed.");    
-//        }
-//        
-//        // We need to convert the IRIS response into KML to render the points
-//        // on the map:
-//       StringBuilder kml = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><name>GML Links to KML</name><description><![CDATA[GeoSciML data converted to KML]]></description>");
-//       
-//       //"<Placemark><name>1</name><description><![CDATA[GENERIC_PARSER:1]]></description><MultiGeometry><Point><Style><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png</href></Icon></IconStyle></Style><coordinates>142.32423823611,-37.75559655,0</coordinates></Point></MultiGeometry></Placemark><Placemark><name>2</name><description><![CDATA[GENERIC_PARSER:2]]></description><MultiGeometry><Point><Style><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png</href></Icon></IconStyle></Style><coordinates>142.515,-37.46436,0</coordinates></Point></MultiGeometry></Placemark>";
-//       
-//        
-//       kml.append("</Document></kml>");
-//        
-//       return generateJSONResponseMAV(true, "gml", kml, null);
-        return generateJSONResponseMAV(true, "gml", "");
+        // Make sure the trailing separator is in place:
+        serviceUrl = serviceUrl.endsWith("/") ? serviceUrl : serviceUrl + "/";
+
+        try {
+            String irisResponse = new Scanner(new URL(serviceUrl + "station/query?net=" + networkCode).openStream(), "ISO-8859-1").useDelimiter("\\A").next();
+
+            DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+            domFactory.setNamespaceAware(true); // never forget this!
+            DocumentBuilder builder = domFactory.newDocumentBuilder();
+            Document irisDoc = builder.parse(new ByteArrayInputStream(irisResponse.getBytes("ISO-8859-1")));
+            NodeList stationEpochs = irisDoc.getDocumentElement().getElementsByTagName("StationEpoch");
+             
+            StringBuilder kml = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><name>GML Links to KML</name><description><![CDATA[GeoSciML data converted to KML]]></description>");
+            
+            // For each station:
+            for (int i = 0; i < stationEpochs.getLength(); i++) {
+                Node stationEpoch = stationEpochs.item(i);
+                Node currentNode = stationEpoch.getFirstChild();
+                
+                String lat = "", lon = "", name = "";
+                
+                do {
+                    String localName = currentNode.getLocalName();
+                    if (localName == "Lat") {
+                        lat = currentNode.getTextContent().trim();
+                    } else if (localName == "Lon") {
+                        lon = currentNode.getTextContent().trim();
+                    } else if (localName == "Site") {
+                        NodeList childNodes = currentNode.getChildNodes();
+                        for (int n = 0; n < childNodes.getLength(); n++) {
+                            if (childNodes.item(n).getNodeName() == "Name") {
+                                name = currentNode.getTextContent().trim();
+                            }
+                        }
+                    }
+                    
+                    currentNode = currentNode.getNextSibling();
+                } while (currentNode != null);
+                
+                // TODO: Sort out paddle!!! Will all this work with open layers??
+                kml.append(
+                        String.format(
+                                "<Placemark><name>%s</name><description><![CDATA[GENERIC_PARSER:%s]]></description><MultiGeometry><Point><Style><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png</href></Icon></IconStyle></Style><coordinates>%s,%s,0</coordinates></Point></MultiGeometry></Placemark>",
+                                name,
+                                i,
+                                lon,
+                                lat));
+            }
+            
+            kml.append("</Document></kml>");
+
+            return generateJSONResponseMAV(true, "gml", kml.toString(), null);
+        } catch (Exception e) {
+            return generateJSONResponseMAV(false, e, "Failed.");
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
