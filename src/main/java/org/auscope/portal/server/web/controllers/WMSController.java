1dc0c7c16e3bca88d068ddef15efc2f56e8b0221
Victor Tey <Victor.Tey@csiro.au>
2013-02-07 06:06:41 +0000

    AUS-2241 : WMS/WFS integration in Auscope portal : attach SLD to getFeatureInfo for more accurate result

--- src/main/java/org/auscope/portal/server/web/controllers/WMSController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/WMSController.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -209,15 +210,16 @@ public class WMSController extends BaseCSWController {
                             @RequestParam("BBOX") String bbox,
                             @RequestParam("WIDTH") String width,
                             @RequestParam("HEIGHT") String height,
-                            @RequestParam("INFO_FORMAT") String infoFormat) throws Exception {
+                            @RequestParam("INFO_FORMAT") String infoFormat,
+                            @RequestParam("SLD") String sld) throws Exception {
 
       String[] bboxParts = bbox.split(",");
       double lng1 = Double.parseDouble(bboxParts[0]);
       double lng2 = Double.parseDouble(bboxParts[2]);
       double lat1 = Double.parseDouble(bboxParts[1]);
       double lat2 = Double.parseDouble(bboxParts[3]);
-
-      String responseString = wmsService.getFeatureInfo(wmsUrl, infoFormat, queryLayers, "EPSG:4326", Math.min(lng1, lng2), Math.min(lat1, lat2), Math.max(lng1, lng2), Math.max(lat1, lat2), Integer.parseInt(width), Integer.parseInt(height), Double.parseDouble(longitude), Double.parseDouble(latitude), Integer.parseInt(x), Integer.parseInt(y), "");
+      String sldDecoded=URLDecoder.decode(sld,"UTF-8");
+      String responseString = wmsService.getFeatureInfo(wmsUrl, infoFormat, queryLayers, "EPSG:4326", Math.min(lng1, lng2), Math.min(lat1, lat2), Math.max(lng1, lng2), Math.max(lat1, lat2), Integer.parseInt(width), Integer.parseInt(height), Double.parseDouble(longitude), Double.parseDouble(latitude), Integer.parseInt(x), Integer.parseInt(y), "",sldDecoded);
       InputStream responseStream = new ByteArrayInputStream(responseString.getBytes());
       this.writeInputToOutputStream(responseStream, response.getOutputStream(), BUFFERSIZE, true);
    }
