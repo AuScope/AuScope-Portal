3acb98fe78a1d192c923db3da6951b3dee73c088
Adam Brown <Adam.Brown@csiro.au>
2013-02-25 12:10:24 +0000

    CSWController.java: Adding support for filtering. WMSController.java: Changed comment to keep consistent with API. UncachedCSWServiceRenderer.js: Lots of changes around dealing with the CSWController interactions. auscope-registries.xml: Tidied up some junk and comments. Still has dev-specific clutter in it.

--- src/main/java/org/auscope/portal/server/web/controllers/CSWController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/CSWController.java
@@ -1,116 +1,108 @@
 package org.auscope.portal.server.web.controllers;
 
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.httpclient.HttpMethodBase;
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+
 import org.auscope.portal.core.server.controllers.BaseCSWController;
-import org.auscope.portal.core.server.controllers.BasePortalController;
 import org.auscope.portal.core.server.http.HttpServiceCaller;
 import org.auscope.portal.core.services.CSWService;
 import org.auscope.portal.core.services.CSWCacheService;
 import org.auscope.portal.core.services.csw.CSWServiceItem;
-import org.auscope.portal.core.services.methodmakers.CSWMethodMakerGetDataRecords;
-import org.auscope.portal.core.services.methodmakers.CSWMethodMakerGetDataRecords.ResultType;
+import org.auscope.portal.core.services.methodmakers.filter.FilterBoundingBox;
+import org.auscope.portal.core.services.methodmakers.filter.csw.CSWGetDataRecordsFilter;
 import org.auscope.portal.core.services.responses.csw.CSWGetRecordResponse;
 import org.auscope.portal.core.services.responses.csw.CSWRecord;
-import org.auscope.portal.core.services.responses.ows.OWSExceptionParser;
-import org.auscope.portal.core.util.DOMUtil;
 import org.auscope.portal.core.view.ViewCSWRecordFactory;
 import org.auscope.portal.core.view.ViewKnownLayerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.w3c.dom.Document;
 
 @Controller
 public class CSWController extends BaseCSWController {
 
     private HttpServiceCaller serviceCaller;
-    private CSWCacheService cswCacheService;
     
     @Autowired
     public CSWController(
             HttpServiceCaller serviceCaller,
             ViewCSWRecordFactory viewCSWRecordFactory,
-            ViewKnownLayerFactory viewKnownLayerFactory,
-            CSWCacheService cswCacheService) {
+            ViewKnownLayerFactory viewKnownLayerFactory) {
         super(viewCSWRecordFactory, viewKnownLayerFactory);
         this.serviceCaller = serviceCaller;
-        this.cswCacheService = cswCacheService;
     }
-    
-    @RequestMapping("/getCSWRecordsNoCache.do")
-    public ModelAndView getCSWRecordsNoCache(
-            String id,
+
+    /**
+     * Requests CSW records from the cswServiceUrl provided.
+     * The results will be filtered by the CQL Text and the filter options.
+     * Records will be returned from the starting point (where 1 is the first record, not 0)
+     * and the number of records retrieved will not exceed maxRecords.
+     * 
+     * @param cswServiceUrl
+     * @param cqlText
+     * // TODO: add filter options.
+     * @param startPoint
+     * @param maxRecords
+     * @return
+     * Example:
+     * "data":[portal.csw.CSWRecord], // These are anonymous objects, you can use them as the config for portal.csw.CSWRecord.
+     * "msg":"No errors",
+     * "totalResults":18, // This is not the number of results returned, it is the number of results the query matched.
+     * "success":true
+     */
+    @RequestMapping("/getUncachedCSWRecords.do")
+    public ModelAndView getUncachedCSWRecords(
             String cswServiceUrl,
             String cqlText,
             int startPoint,
-            int maxRecords) {
-        System.out.println(cswCacheService.toString());
-        
-        List<CSWRecord> records = null;
-        
+            int maxRecords,
+            String bbox,
+            String anyText,
+            String title,
+            String abstract_            
+            ) {
         CSWServiceItem endpoint = new CSWServiceItem(
-                id,
+                "", // This ID won't actually be used so we can just leave it blank. 
                 cswServiceUrl);
         endpoint.setCqlText(cqlText);
-        
+
         CSWService cswService = new CSWService(
                 endpoint,
                 this.serviceCaller,
                 false);
 
         try {
-            CSWGetRecordResponse response = cswService.queryCSWEndpoint(1, 1);
+            FilterBoundingBox spatialBounds = FilterBoundingBox.attemptParseFromJSON(bbox);
             
-            System.out.println(response.getRecordsMatched());
+            // TODO: REMOVE
+            System.out.println(spatialBounds);
+            System.out.println(bbox);
             
-            records = response.getRecords();
+            CSWGetDataRecordsFilter filter = new CSWGetDataRecordsFilter(
+                    anyText,
+                    spatialBounds); 
+            
+            filter.setTitle(title);
+            filter.setAbstract(abstract_);
+            
+            CSWGetRecordResponse response = cswService.queryCSWEndpoint(
+                    startPoint, 
+                    maxRecords,
+                    filter);
+            
+            List<CSWRecord> records = response.getRecords();
+            
+            return generateJSONResponseMAV(
+                    records.toArray(new CSWRecord[records.size()]),
+                    response.getRecordsMatched());
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         
-//        CSWServiceClient cswServiceClient = 
-//                new CSWServiceClient(
-//                        /*CSWServiceItem*/);
-//        
-        
-//        CSWServiceItem endpoint = new CSWServiceItem(
-//                /* TODO: ADAM: I DON'T KNOW WHAT ID SHOULD BE? */"",
-//                cswServiceUrl);
-//        
-////        endpoint.setCqlText(cqlText);
-//        
-//        
-//        CSWMethodMakerGetDataRecords methodMaker = new CSWMethodMakerGetDataRecords();
-//        try {
-//            HttpMethodBase method = methodMaker.makeMethod(
-//                    endpoint.getServiceUrl(), 
-//                    null, 
-//                    ResultType.Results, 
-//                    /*MAX_QUERY_LENGTH*/ 10, 
-//                    1,
-//                    endpoint.getCqlText());
-//            
-//            InputStream responseStream = serviceCaller.getMethodResponseAsStream(method);
-//            Document responseDocument = DOMUtil.buildDomFromStream(responseStream);
-//            OWSExceptionParser.checkForExceptionResponse(responseDocument);
-//            CSWGetRecordResponse response = new CSWGetRecordResponse(endpoint, responseDocument);
-//            
-//            records = response.getRecords();
-//        } catch (UnsupportedEncodingException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-        
-        return generateJSONResponseMAV(records.toArray(new CSWRecord[records.size()]));
+        return generateJSONResponseMAV(false);
     }
 }
--- src/main/webapp/js/auscope/layer/renderer/cswservice/UncachedCSWServiceRenderer.js
+++ src/main/webapp/js/auscope/layer/renderer/cswservice/UncachedCSWServiceRenderer.js
@@ -1,9 +1,11 @@
 /**
- * An implementation of a portal.layer.renderer for controlling access to a CSW service endpoint
- * that hasn't been pre-cached.
+ * An implementation of a portal.layer.renderer for controlling access to a CSW
+ * service endpoint that hasn't been pre-cached.
  */
 Ext.define('portal.layer.renderer.cswservice.UncachedCSWServiceRenderer', {
-    extend: 'portal.layer.renderer.Renderer',
+    extend : 'portal.layer.renderer.Renderer',
+
+    _maximumCSWRecordsToRetrieveAtOnce : 100,
 
     config : {
         /**
@@ -12,97 +14,148 @@ Ext.define('portal.layer.renderer.cswservice.UncachedCSWServiceRenderer', {
         icon : null
     },
 
-    constructor: function(config) {
-//        this.legend = Ext.create('portal.layer.legend.wfs.WFSLegend', {
-//            iconUrl : config.icon ? config.icon.getUrl() : ''
-//        });
-        
+    constructor : function(config) {
+        // this.legend = Ext.create('portal.layer.legend.wfs.WFSLegend', {
+        // iconUrl : config.icon ? config.icon.getUrl() : ''
+        // });
+
         // Call our superclass constructor to complete construction process.
         this.callParent(arguments);
     },
-    
+
     /**
-     * An implementation of the abstract base function. See comments in superclass
-     * for more information.
-     *
+     * An implementation of the abstract base function. See comments in
+     * superclass for more information.
+     * 
      * function(portal.csw.OnlineResource[] resources,
-     *          portal.layer.filterer.Filterer filterer,
-     *          function(portal.layer.renderer.Renderer this, portal.csw.OnlineResource[] resources, portal.layer.filterer.Filterer filterer, bool success) callback
-     *
+     * portal.layer.filterer.Filterer filterer,
+     * function(portal.layer.renderer.Renderer this, portal.csw.OnlineResource[]
+     * resources, portal.layer.filterer.Filterer filterer, bool success)
+     * callback
+     * 
      * returns - void
-     *
+     * 
      * resources - an array of data sources which should be used to render data
-     * filterer - A custom filter that can be applied to the specified data sources
-     * callback - Will be called when the rendering process is completed and passed an instance of this renderer and the parameters used to call this function
+     * filterer - A custom filter that can be applied to the specified data
+     * sources callback - Will be called when the rendering process is completed
+     * and passed an instance of this renderer and the parameters used to call
+     * this function
      */
     displayData : function(resources, filterer, callback) {
-                
+
         // This method is called at the time the user presses "Apply Filter >>".
-        // You need to do the following things:        
+        // You need to do the following things:
         // 
-        // 1. Make the request for the CSW records with the specified filter but tell it you only want 1 record.
-        // 2. Use that record's 'total number of records' field to decide whether or not you should request the rest of them.
-        //      3. If numberOfRecords < $someLimit 
-        //          then: just get them all (you can just request from position 2 onwards since you already have the 1st).
-        //          else: ask the user if they want to get them all or just get some
-        // 4. Render the responses. TODO: Will they be rendered as CSWRecords or should the CSWOnlineResources be used
-        //    to determine which specialised renderer to use?
+        // 1. Make the request for the CSW records with the specified filter but
+        // tell it you only want 1 record.
+        // 2. Use that record's 'total number of records' field to decide
+        // whether or not you should request the rest of them.
+        // 3. If numberOfRecords < $someLimit
+        // then: just get them all (you can just request from position 2 onwards
+        // since you already have the 1st).
+        // else: ask the user if they want to get them all or just get some
+        // 4. Render the responses. TODO: Will they be rendered as CSWRecords or
+        // should the CSWOnlineResources be used
+        // to determine which specialised renderer to use?
 
         // Here's step 1:
-//        var running = true;
+        // var running = true;
         var cswRecord = this.parentLayer.data.cswRecords[0].data;
-        var id = cswRecord.id;
         var cswServiceUrl = resources[0].data.url;
-        var cqlText = cswRecord.descriptiveKeywords[0]; 
+        var cqlText = cswRecord.descriptiveKeywords[0];
         
+        var bboxFilter = filterer.spatialParam;
+        var anyTextFilter = filterer.parameters.anyText;
+        var titleFilter = filterer.parameters.title;
+        var abstractFilter = filterer.parameters.abstract;
+        
+
         _ajaxRequest = Ext.Ajax.request({
-            url : 'getCSWRecordsNoCache.do',
+            url : 'getUncachedCSWRecords.do',
             params : {
-                id : id,
                 cswServiceUrl : cswServiceUrl,
                 cqlText : cqlText,
                 startPoint : 1,
-                maxRecords : 100 // TODO: sort out these two things.
+                maxRecords : this._maximumCSWRecordsToRetrieveAtOnce,
+                bbox : Ext.JSON.encode(bboxFilter),
+                anyText : anyTextFilter,
+                title : titleFilter,
+                abstract_ : abstractFilter
+            // TODO: add filtering bit here for date ranges.
             },
+            scope: this,
             success : function(response) {
                 response = Ext.JSON.decode(response.responseText);
                 if (response.success) {
                     cswRecords = [];
-                    
+
                     for (i = 0; i < response.data.length; i++) {
-                        cswRecords.push(Ext.create('portal.csw.CSWRecord', response.data[i]));
+                        cswRecords.push(Ext.create('portal.csw.CSWRecord',
+                                response.data[i]));
+                    }
+
+                    var recordsReturned = cswRecords.length;
+                    var recordsAvailable = response.totalResults;
+
+                    if (recordsReturned <= recordsAvailable) {
+                        
+                        this.parentLayer.set('cswRecords', cswRecords);
+                        
+                        // Just show the results straight away:
+                        var cswRenderer = Ext.create('portal.layer.renderer.csw.CSWRenderer', {
+                            map : this.map,
+                            icon : this.icon,
+                            polygonColor: this.polygonColor
+                        });
+                        
+                        cswRenderer.parentLayer = this.parentLayer;
+                        
+                        var wholeGlobe = Ext.create('portal.util.BBox', {
+                            northBoundLatitude : 90,
+                            southBoundLatitude : -90,
+                            eastBoundLongitude : -180,
+                            westBoundLongitude : 180                            
+                        });
+                        
+                        var emptyFilter = Ext.create('portal.layer.filterer.Filterer', {
+                            spatialParam : wholeGlobe}); 
+                        
+                        cswRenderer.displayData(cswRecords, 
+                                emptyFilter,
+                                undefined);
+                    } else {
+                        // We have to ask the user what they want to do.
                     }
                 }
             }
         });
-
-        
-//        while(running); // TODO: ADAM: FIX THIS
-//        this.parentLayer.set('cswRecords', cswRecords);
     },
 
     /**
-     * An abstract function for creating a legend that can describe the displayed data. If no
-     * such thing exists for this renderer then null should be returned.
-     *
+     * An abstract function for creating a legend that can describe the
+     * displayed data. If no such thing exists for this renderer then null
+     * should be returned.
+     * 
      * function(portal.csw.OnlineResource[] resources,
-     *          portal.layer.filterer.Filterer filterer)
-     *
+     * portal.layer.filterer.Filterer filterer)
+     * 
      * returns - portal.layer.legend.Legend or null
-     *
-     * resources - (same as displayData) an array of data sources which should be used to render data
-     * filterer - (same as displayData) A custom filter that can be applied to the speHoucified data sources
+     * 
+     * resources - (same as displayData) an array of data sources which should
+     * be used to render data filterer - (same as displayData) A custom filter
+     * that can be applied to the speHoucified data sources
      */
     getLegend : function(resources, filterer) {
         return this.legend;
     },
 
     /**
-     * An abstract function that is called when this layer needs to be permanently removed from the map.
-     * In response to this function all rendered information should be removed
-     *
+     * An abstract function that is called when this layer needs to be
+     * permanently removed from the map. In response to this function all
+     * rendered information should be removed
+     * 
      * function()
-     *
+     * 
      * returns - void
      */
     removeData : function() {
