e42867b2e4579181921ae8a7a3280ad5e6229f0c
Adam Brown <Adam.Brown@csiro.au>
2013-03-27 03:25:04 +0000

    AGOS-15: Added comments.

--- src/main/java/org/auscope/portal/view/knownlayer/IRISSelector.java
+++ src/main/java/org/auscope/portal/view/knownlayer/IRISSelector.java
@@ -8,14 +8,32 @@ import org.auscope.portal.core.services.responses.csw.AbstractCSWOnlineResource.
 import org.auscope.portal.core.services.responses.csw.CSWRecord;
 import org.auscope.portal.core.view.knownlayer.KnownLayerSelector;
 
+/**
+ * The IRISSelector class provides functionality to determine the type of relationship, if any,
+ * that exists between a service endpoint and a CSWRecord. 
+ * @author Adam
+ *
+ */
 public class IRISSelector implements KnownLayerSelector {
     
+    /**
+     * The service endpoint that the instance of the selector is concerned with.
+     */
     private URL serviceEndpoint;
     
+    /**
+     * Initialises a new instance of the IRISSelector class.
+     * @param serviceEndpoint The service endpoint that the instance of the selector is concerned with.
+     * @throws MalformedURLException
+     */
     public IRISSelector(String serviceEndpoint) throws MalformedURLException {
         this.serviceEndpoint = new URL(serviceEndpoint);
     }
     
+    /**
+     * Returns a RelationType enum that indicates the relationship between the record provided
+     * and the service endpoint that this IRISSelector was instantiated with. 
+     */
     @Override
     public RelationType isRelatedRecord(CSWRecord record) {
         AbstractCSWOnlineResource[] onlineResources = record.getOnlineResourcesByType(OnlineResourceType.IRIS);
