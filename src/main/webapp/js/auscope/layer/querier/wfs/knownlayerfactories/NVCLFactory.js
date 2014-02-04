01a6f91cd86e2bcaa8202b2fcf9364c97ea05481
Victor Tey <Victor.Tey@csiro.au>
2014-02-04 06:10:19 +0000

    testing google analytic trackevent

--- src/main/webapp/js/auscope/layer/querier/wfs/knownlayerfactories/NVCLFactory.js
+++ src/main/webapp/js/auscope/layer/querier/wfs/knownlayerfactories/NVCLFactory.js
@@ -346,7 +346,7 @@ Ext.define('auscope.layer.querier.wfs.knownlayerfactories.NVCLFactory', {
                                         for(var i=0;i<datasetIds.length;i++){
                                             logIds[i] =  datasetIds[i].get('logId');
                                         }
-                                        _gaq.push(['_trackEvent', 'NVCL', 'DOWNLOAD', 'CSV Download:'+nvclDataServiceUrl,logIds]);
+                                        _gaq.push(['_trackEvent', 'NVCL', 'DOWNLOAD', 'CSV Download:'+nvclDataServiceUrl,logIds.join()]);
                                         portal.util.FileDownloader.downloadFile('getNVCL2_0_CSVDownload.do', {
                                             serviceUrl : nvclDataServiceUrl,
                                             logIds : logIds
