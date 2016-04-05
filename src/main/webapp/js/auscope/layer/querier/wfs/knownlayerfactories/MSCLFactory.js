443e01359a0825f5c4cf7dd04311736b892e2f92
Adam Brown <adam.brown@evbane.com>
2016-04-05 11:23:00 +0800

    AUS-2626: Added support for log scale in MSCL graph.

--- src/main/webapp/js/auscope/layer/querier/wfs/knownlayerfactories/MSCLFactory.js
+++ src/main/webapp/js/auscope/layer/querier/wfs/knownlayerfactories/MSCLFactory.js
@@ -4,6 +4,109 @@
 Ext.require('Ext.chart.*');
 Ext.require([ 'Ext.Window', 'Ext.fx.target.Sprite', 'Ext.layout.container.Fit', 'Ext.window.MessageBox' ]);
 
+Ext.define('Extjs.contribs.chart.axis.layout.Logarithmic', {
+  extend: 'Ext.chart.axis.layout.Continuous',
+  alias: 'axisLayout.logarithmic',
+  
+  config: {
+    adjustMinimumByMajorUnit: false,
+    adjustMaximumByMajorUnit: false
+  },
+  
+  /**
+   * Convert the value from the normal to the log10 version.
+   * NB: The renderer for the field needs to do the inverse.
+   * It is also advised that 0 and negative values be excluded, as they will return negative Infinity
+   */
+  getCoordFor: function (value, field, idx, items) {
+    return Math.log10(value);
+  },
+
+  /**
+   * Called by the parent class to build the 'tick marks', which also determines where the grid lines get shown.
+   * The default behaviour is to get an even spread. Instead, we want to show the grid lines on the multiples of the
+   * power of tens - e.g. 1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100, and so forth
+   */
+  snapEnds: function (context, min, max, estStepSize) {
+    var segmenter = context.segmenter;
+    var axis = this.getAxis();
+    var majorTickSteps = axis.getMajorTickSteps();
+        // if specific number of steps requested and the segmenter can do such segmentation
+    var out = majorTickSteps && segmenter.exactStep ?
+        segmenter.exactStep(min, (max - min) / majorTickSteps) :
+        segmenter.preferredStep(min, estStepSize);
+    var unit = out.unit;
+    var step = out.step;
+    var from = segmenter.align(min, step, unit);
+
+    // Calculate the steps.
+    var steps = []
+    for (var magnitude = Math.floor(min); magnitude < Math.ceil(max); magnitude++) {
+      var baseValue = Math.pow(10, magnitude);
+      for (var increment = 1; increment < 10; increment++) {
+        var value = baseValue * increment;
+        var logValue = Math.log10(value);
+        if (logValue > min && logValue < max) {
+          steps.push(logValue);
+        }
+      }
+    }
+
+    return {
+        min: segmenter.from(min),
+        max: segmenter.from(max),
+        from: from,
+        to: segmenter.add(from, steps * step, unit),
+        step: step,
+        steps: steps.length,
+        unit: unit,
+        get: function (current) {
+            return steps[current]
+        }
+    };
+  },
+  
+  // Trimming by the range makes the graph look weird. So we don't.
+  trimByRange: Ext.emptyFn
+  
+}, function() {
+  // IE (and the PhantomJS system) do not have have a log10 function. So we polyfill it in if needed.
+  Math.log10 = Math.log10 || function(x) {
+    return Math.log(x) / Math.LN10;
+  };
+})
+
+Ext.define('Extjs.contribs.chart.axis.segmenter.Logarithmic', {
+  extend: 'Ext.chart.axis.segmenter.Numeric',
+  alias: 'segmenter.logarithmic',
+  config: {
+    minimum: 200
+  },
+  
+  renderer: function (value, context) {
+    return (Math.pow(10, value)).toFixed(3);
+  }
+
+});
+
+Ext.define('Extjs.contribs.chart.axis.Logarithmic', {
+  extend: 'Ext.chart.axis.Numeric',
+  
+  requires: [
+    'Extjs.contribs.chart.axis.layout.Logarithmic',
+    'Extjs.contribs.chart.axis.segmenter.Logarithmic'
+  ],
+  
+  type: 'logarithmic',
+  alias: [
+    'axis.logarithmic',
+  ],
+  config: {
+    layout: 'logarithmic',
+    segmenter: 'logarithmic',
+  }
+});
+
 function drawGraph(serviceUrl, boreholeHeaderId, startDepth, endDepth, observationsToReturn, maskedElement) {
     // I want this to always be an array, even if it only has one element.
     observationsToReturn = [].concat(observationsToReturn);
@@ -37,6 +140,8 @@ function drawGraph(serviceUrl, boreholeHeaderId, startDepth, endDepth, observati
             for (var i = 0; i < observationsToReturn.length; i++) {
                 var first = i === 0;
                 var last = i === observationsToReturn.length - 1;
+                
+                var xAxisType = ['magnetic_susceptibility', 'resistivity'].indexOf(observationsToReturn[i]) == -1 ? 'numeric' : 'logarithmic';
 
                 var xAxisTitle =
                     observationsToReturn[i] == 'diameter' ? 'Diameter (cm)' :
@@ -113,7 +218,7 @@ function drawGraph(serviceUrl, boreholeHeaderId, startDepth, endDepth, observati
                         // End ReverseAxisIssue.
                     }, {
                         title : xAxisTitle,
-                        type : 'numeric',
+                        type : xAxisType,
                         position : 'top',
                         fields : [ observationsToReturn[i] ],
                         minorTickSteps : 1
