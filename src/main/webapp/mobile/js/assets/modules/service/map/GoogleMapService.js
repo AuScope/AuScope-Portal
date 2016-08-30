27d91f7756524b36628e68738535b9b702e52da8
Victor Tey <victor.tey@csiro.au>
2016-08-30 15:40:55 +0800

    tune web on mobile

--- src/main/webapp/mobile/js/assets/modules/service/map/GoogleMapService.js
+++ src/main/webapp/mobile/js/assets/modules/service/map/GoogleMapService.js
@@ -5,20 +5,22 @@ allModules.service('googleMapService',['$rootScope',function ($rootScope) {
     this.getMap = function(){
         return this.mainMap;
     };
+    
+   
      
   
      this.initMap = function() {
+         var mq = window.matchMedia( "(max-width: 658px)" );
          this.mainMap = new google.maps.Map(document.getElementById('google-map-main'), {
            center: {lat: -28.397, lng: 132.644},
-           zoom: 5,
+           zoom: (mq.matches? 3 : 5),
            mapTypeControlOptions: {
                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU ,
                position: google.maps.ControlPosition.TOP_RIGHT
            },
          });
-         
-       
-      }
+
+      };
    
     
     
