444f8b050ebd4c6992b493ea6a0027a565b963ac
Vincent Fazio <vincent.fazio@csiro.au>
2016-09-07 12:51:35 +1000

    AUS-2738 Updated source code documentation

--- src/main/webapp/mobile/doc/out/api.js
+++ src/main/webapp/mobile/doc/out/api.js
@@ -1,24 +1,37 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "CollapseDemoCtrl",
         "GMLParserService",
         "GetCSWRecordService",
         "GetWFSRelatedService",
         "GetWMSRelatedService",
         "GoogleMapService",
         "LayerManagerService",
+        "PreviewMapService",
         "RenderHandlerService",
         "SimpleXPathService",
         "WFSService",
-        "WMSService"
+        "WMSService",
+        "defaultFilterCtrl",
+        "googleMapCtrl",
+        "infoPanelCtrl",
+        "layerPanelCtrl",
+        "loadFilterCtrl"
     ],
     "modules": [
+        "controllers",
         "http",
         "layer",
         "map",
         "utility"
     ],
     "allModules": [
+        {
+            "displayName": "controllers",
+            "name": "controllers",
+            "description": "defaultFilterCtrl class used to add layers to the main map"
+        },
         {
             "displayName": "http",
             "name": "http",
