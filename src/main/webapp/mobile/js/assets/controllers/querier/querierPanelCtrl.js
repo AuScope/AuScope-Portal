f86ea78da39060ebe7cd154e0ae866057de2fa4f
Vincent Fazio <vincent.fazio@csiro.au>
2016-10-27 11:55:03 +1100

    AUS-2744 Two querier panel fixes: (i) Converted expandall/collapseall buttons into one toggle button (ii) Made some nodes in accordion tree start in open state to make it easy to read

--- src/main/webapp/mobile/js/assets/controllers/querier/querierPanelCtrl.js
+++ src/main/webapp/mobile/js/assets/controllers/querier/querierPanelCtrl.js
@@ -7,18 +7,38 @@
 allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPanelService', function ($compile, $scope, QuerierPanelService) {
     
     /**
-    * Registers and returns a random variable name, used for storing the state of all the segments of the accordion 
-    * @method genRandomVarName
+    * Used to initialise the controller.
+    * @method init
+    * @param xmlPanelId the id of the <div> where the XML accordion tree is to be displayed
     */
-    $scope.genRandomVarName = function () {
+    $scope.init = function(xmlPanelId) { 
+        $scope.xmlPanelId = xmlPanelId;
+        // This stores the open/closed state of each accordion node
+        $scope.status = {};
+        // This stores the state of the "Expand All"/"Collapse All" toggle button
+        $scope.expandFlag = false;
+        // This registers this class' functions with the QuerierPanelService
+        QuerierPanelService.registerPanel($scope.openPanel, $scope.setPoint, $scope.setPanelTree);
+    };
+    
+    
+    /**
+    * Creates a random variable name, inserts as attribute name in '$scope.status', and sets the attribute value to 'statusVal'
+    * This routine is used to store the open/closed state of all the accordion nodes
+    * @method genRandomVarName
+    * @param statusVal value of the open/closed state
+    * @return random variable name
+    */
+    $scope.genRandomVarName = function (statusVal) {
         // Create var name
         var varName = "openSubQueryPanel_"+Math.floor(Math.random()*10000000).toString();
-        // Register var name
-        $scope.status[varName] = false;
+        // Register and set var name
+        $scope.status[varName] = statusVal;
         // Return var name
         return varName;
     }
-    
+
+
     /**
     * Used to display point in panel
     * Passed to 'QuerierPanelService' as a parameter, then called by 'QuerierPanelService'
@@ -37,22 +57,33 @@ allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPa
     * @method parse
     * @param xmlDoc XML DOM object to be parsed
     * @param parentDiv div used to append dom objects to make the accordion
+    * @param parentStatusVarName var name of parent as used the '$scope.status' object
     */        
-    $scope.parse = function(xmlDoc, parentDiv) {
+    $scope.parse = function(xmlDoc, parentDiv, parentStatusVarName) {
         var body = xmlDoc.textContent && xmlDoc.textContent.trim(),
         hasChildren = xmlDoc.childNodes && xmlDoc.childNodes.length,
         hasAttributes = xmlDoc.attributes && xmlDoc.attributes.length;
         
-        // If it's text node, then do not make accordion, just append a textNode to the parentDiv
+        // If it's text XML node, then do not make accordion, just append a textNode to the parentDiv
         if (xmlDoc.nodeName=='#text') {
             var textNode = document.createTextNode(body);
             parentDiv.appendChild(textNode);
+            // Leaf accordion nodes should start in opened state
+            $scope.status[parentStatusVarName] = true;            
             return;
         }
 
         // Do not display attributes for 'wfs:FeatureCollection'
         if (xmlDoc.nodeName=='wfs:FeatureCollection') {
             hasAttributes = false;
+        }
+        
+        // Default accordion state is closed
+        var isOpenVal=false;
+        
+        // But some accordion nodes are open at first
+        if (xmlDoc.nodeName=='gml:featureMembers' || xmlDoc.nodeName=='wfs:FeatureCollection') {
+            isOpenVal = true;
         }
         
         // Set up 'accordion'
@@ -62,8 +93,8 @@ allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPa
         // Set up 'accordion-heading' 
         var headingDiv = document.createElement('uib-accordion-heading');
         
-        // Set up chevron icon
-        var varName = $scope.genRandomVarName();
+        // Set up chevron icon and open/close state of accordion node
+        var varName = $scope.genRandomVarName(isOpenVal);
         var iconElem = document.createElement('i');
         iconElem.className = "glyphicon glyphicon-plus";
         iconElem.setAttribute("ng-class", "{'glyphicon-chevron-down': status."+varName+", 'glyphicon-chevron-right': !status."+varName+"}");
@@ -110,7 +141,7 @@ allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPa
         if (hasChildren) {
             // Go through all the child nodes, recursively call parse()
             for (var i=0; i<xmlDoc.childNodes.length; i++) {
-                $scope.parse(xmlDoc.childNodes[i], groupDiv);
+                $scope.parse(xmlDoc.childNodes[i], groupDiv, varName);
             }
         } else {
             var textNode = document.createTextNode(body);
@@ -134,7 +165,7 @@ allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPa
                     startNode.removeChild(startNode.lastChild);
                 }
                 // Call parse() to parse the XML tree, and create accordion DOM objects for display
-                $scope.parse(XML.childNodes[0], startNode);
+                $scope.parse(XML.childNodes[0], startNode, "root");
                 $compile(startNode)($scope);
             }
         } else {
@@ -147,6 +178,7 @@ allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPa
     * Passed to 'QuerierPanelService' as a parameter, then called by 'QuerierPanelService'
     * @method openPanel 
     * @param ctrlBool if true panel will open, if false panel will close
+    * @param useApply will call $apply() on $parent object if set to true
     */
     $scope.openPanel = function(ctrlBool, useApply) {
         $scope.$parent.showQuerierPanel = ctrlBool;
@@ -154,39 +186,32 @@ allControllers.controller('querierPanelCtrl',  ['$compile', '$scope', 'QuerierPa
             $scope.$parent.$apply();
     }
 
-    /**
-    * Used to initialise the controller.
-    * @method init
-    * @param xmlPanelId the id of the <div> where the XML tree is to be displayed
-    */
-    $scope.init = function(xmlPanelId) {
-        $scope.xmlPanelId = xmlPanelId;
-        $scope.status = {};
-        QuerierPanelService.registerPanel($scope.openPanel, $scope.setPoint, $scope.setPanelTree);
-    };
-	
-	/**
-	* Used to expand all the accordion elements in the panel at once
-	* @method expandAll
-	*/
-	$scope.expandAll = function() {
-		var varName;
-	    for (varName in $scope.status) {
-			$scope.status[varName] = true;
-		} 
-		
-	};
 
-	/**
-	* Used to collapse all the accordion elements in the panel at once
-	* @method collapseAll
-	*/
-	$scope.collapseAll = function() {
-		var varName;
-	    for (varName in $scope.status) {
-			$scope.status[varName] = false;
-		}
-	};
+    
+    /**
+    * Used to expand all the accordion elements in the panel at once
+    * @method expandAll
+    */
+    $scope.expandAll = function() {
+        var varName;
+        $scope.expandFlag = true;
+        for (varName in $scope.status) {
+            $scope.status[varName] = true;
+        } 
+        
+    };
+
+    /**
+    * Used to collapse all the accordion elements in the panel at once
+    * @method collapseAll
+    */
+    $scope.collapseAll = function() {
+        var varName;
+        $scope.expandFlag = false;
+        for (varName in $scope.status) {
+            $scope.status[varName] = false;
+        }
+    };
 
 
 }]);
