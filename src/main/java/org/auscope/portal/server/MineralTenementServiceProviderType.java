9b7a9b63d202c25096439d0d63852729f2ea07ba
Vincent Fazio <vincent.fazio@csiro.au>
2016-11-15 14:24:39 +1100

    AUS-2759 Add ability for Mineral Tenements layer to use ArcGIS server by merging in commit 454b551d7bac9c6d177f60855760c76a91747f8c from 'geoscience-portal'

--- src/main/java/org/auscope/portal/server/MineralTenementServiceProviderType.java
+++ src/main/java/org/auscope/portal/server/MineralTenementServiceProviderType.java
@@ -1,47 +1,61 @@
 package org.auscope.portal.server;
 
+/**
+ * Differentiates WMS service providers for Mineral Tenement services.
+ * 
+ * The behaviour of ArcGIS Server is different to the default (GeoServer) and this
+ * enum provides settings for each such that they behave in the same way to the
+ * end user.
+ * 
+ */
 public enum MineralTenementServiceProviderType {
-    GeoServer("mt:MineralTenement", "#66ff66", "mt:name", "mt:owner", "mt:shape"), 
-    ArcGIS("MineralTenement", "#00ff00", "TENNAME", "TENOWNER", "SHAPE");
+    GeoServer("mt:MineralTenement", "#66ff66", "#a3ffa3", "mt:name", "mt:owner", "mt:shape"), 
+    ArcGIS("MineralTenement", "#00ff00", "#66ff66", "TENNAME", "TENOWNER", "SHAPE");
 
-    private final String colour;
     private final String featureType;
-    private final String tenementName;
-    private final String owner;
-    private String shape;
+    private final String fillColour;
+    private final String borderColour;
+    private final String nameField;
+    private final String ownerField;
+    private String shapeField;
 
-    private MineralTenementServiceProviderType(String featureType, String colour, String tenementName, String owner,
+    private MineralTenementServiceProviderType(String featureType, String fillColour, String borderColour, String tenementName, String owner,
                 String shape) {
         this.featureType = featureType;
-        this.colour = colour;
-        this.tenementName = tenementName;
-        this.owner = owner;
-        this.shape = shape;
-    }
-
-    public String colour() {
-        return colour;
+        this.fillColour = fillColour;
+        this.borderColour = borderColour;
+        this.nameField = nameField;
+        this.ownerField = ownerField;
+        this.shapeField = shapeField;
     }
 
     public String featureType() {
         return featureType;
     }
 
-    public String tenementName() {
-        return tenementName;
+    public String fillColour() {
+        return fillColour;
     }
 
-    public String owner() {
-        return owner;
+    public String borderColour() {
+        return borderColour;
     }
 
-    public String shape() {
-        return shape;
+    public String nameField() {
+        return nameField;
+    }
+
+    public String ownerField() {
+        return ownerField;
+    }
+
+    public String shapeField() {
+        return shapeField;
     }
 
     public static MineralTenementServiceProviderType parseUrl(String serviceUrl) {
-        if (serviceUrl.toUpperCase().contains("MAPSERVER/WFSSERVER")
-                || serviceUrl.toUpperCase().contains("MAPSERVER/WMSSERVER")) {
+        if (serviceUrl != null && (serviceUrl.toUpperCase().contains("MAPSERVER/WFSSERVER")
+                || serviceUrl.toUpperCase().contains("MAPSERVER/WMSSERVER"))) {
             return MineralTenementServiceProviderType.ArcGIS;
         }
         return MineralTenementServiceProviderType.GeoServer;
