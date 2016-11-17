dad9b1ba97e33df21522ccff01e069873faa75f3
Victor Tey <victor.tey@csiro.au>
2016-11-17 14:14:16 +0800

    add utilities to split url into parameters

--- src/main/webapp/mobile/js/assets/modules/service/utility/UtilitiesService.js
+++ src/main/webapp/mobile/js/assets/modules/service/utility/UtilitiesService.js
@@ -7,6 +7,79 @@
  * 
  */
 allModules.service('UtilitiesService',['$rootScope',function ($rootScope) {
+    
+    /**
+     * Test if string s contains c
+     * @method stringContains
+     * @param s - the string to check
+     * @param c - the string to match
+     */
+    this.stringContains = function(s,c){
+        return s.indexOf(c) != -1;
+    };
+    
+    /**
+     * Returns the parameter in a get url request
+     * @method getUrlParameters
+     * @param url - the get url string to break
+     * @param options - splitArgs - {Boolean} Split comma delimited params into arrays? Default is true
+     */
+    this.getUrlParameters = function(url, options) {
+        var localStringContain  = function(s,c){
+            return s.indexOf(c) != -1;
+        };
+        options = options || {};
+        // if no url specified, take it from the location bar
+        url = (url === null || url === undefined) ? window.location.href : url;
+
+        //parse out parameters portion of url string
+        var paramsString = "";
+        if (localStringContain(url,"?")) {
+            var start = url.indexOf('?') + 1;
+            var end = localStringContain(url, "#") ?
+                        url.indexOf('#') : url.length;
+            paramsString = url.substring(start, end);
+        }
+
+        var parameters = {};
+        var pairs = paramsString.split(/[&;]/);
+        for(var i=0, len=pairs.length; i<len; ++i) {
+            var keyValue = pairs[i].split('=');
+            if (keyValue[0]) {
+
+                var key = keyValue[0];
+                try {
+                    key = decodeURIComponent(key);
+                } catch (err) {
+                    key = unescape(key);
+                }
+                
+                // being liberal by replacing "+" with " "
+                var value = (keyValue[1] || '').replace(/\+/g, " ");
+
+                try {
+                    value = decodeURIComponent(value);
+                } catch (err) {
+                    value = unescape(value);
+                }
+                
+                // follow OGC convention of comma delimited values
+                if (options.splitArgs !== false) {
+                    value = value.split(",");
+                }
+
+                //if there's only one value, do not return as array                    
+                if (value.length == 1) {
+                    value = value[0];
+                }                
+                
+                parameters[key] = value;
+             }
+         }
+        return parameters;
+    };
+    
+   
    
     /**
      * Test if the object is empty
