52e36e7a32fa8114253ae185b02a84ed201060ca
Vincent Fazio <vincent.fazio@csiro.au>
2016-12-21 13:03:37 +1100

    AUS-2771 Changes: 1) Added comments and realigned text with 4-space tabs 2) Added BootStrap dropdowns to replace <select> 3) Many changes for small screen devices

--- src/main/webapp/mobile/js/assets/controllers/filters/loadFilterCtrl.js
+++ src/main/webapp/mobile/js/assets/controllers/filters/loadFilterCtrl.js
@@ -6,36 +6,75 @@
 allControllers.controller('loadFilterCtrl', ['$scope','$rootScope','$timeout','RenderHandlerService','LayerManagerService','UtilitiesService','GetFilterParamService',
                                              function ($scope,$rootScope,$timeout,RenderHandlerService,LayerManagerService,UtilitiesService,GetFilterParamService) {
     
-   $scope.optionalFilters=[];
-   $scope.providers=[];
-   $scope.select={
-           filter:{}
-   };
-  
-  
+    /* Optional filters for display in panel */
+    $scope.optionalFilters=[];
+    
+    /* Providers used for display in panel */
+    $scope.providers=[];
+    
+    /* Value displayed in the optional dropdown selector */ 
+    $scope.optDropdownSelectLabel = "-- choose an option --";
+    
+    /* Value displayed in the remote dropdown selector */
+    $scope.dropdownRemoteLabel = "-- choose an option --";
+    
+    /* Value displayed in the mandatory dropdown selector */ 
+    $scope.mandDropdownSelectLabel =  "-- choose an option --";
+    
+    /**
+     * Sets the value displayed in the optional dropdown selector
+     * @method setOptSelectList
+     * @param val value to be set in dropdown selector
+     */
+    $scope.setOptSelectList = function(val) {
+        var setVal = val;
+        $scope.optDropdownSelectLabel = setVal;
+    }
+    
+    /**
+     * Sets the value displayed in the mandatory dropdown selector
+     * @method setMandSelectList
+     * @param val value to be set in dropdown selector
+     */
+    $scope.setMandSelectList = function(val) {
+        var setVal = val;
+        $scope.mandDropdownSelectLabel = setVal;
+    }
    
-   var getProvider = function(){
-       var cswRecords = $scope.layer.cswRecords;
+    /**
+     * Sets the value displayed in the remote dropdown selector
+     * @method setDropdownRemote
+     * @param val value to be set in dropdown selector
+     */
+    $scope.setDropdownRemote = function(val) {
+        var setVal = val;
+        $scope.dropdownRemoteLabel = setVal;
+    }
+   
+    /**
+     * Assembles a list of providers, which will be displayed in the panel
+     * @method getProvider
+     */
+    var getProvider = function(){
+        var cswRecords = $scope.layer.cswRecords;
        
-     //Set up a map of admin areas + URL's that belong to each
-       var adminAreasMap = {};
-       for (var i = 0; i < cswRecords.length; i++) {
-           var adminArea = cswRecords[i]['adminArea'];
-           var allOnlineResources = LayerManagerService.getOnlineResourcesFromCSW(cswRecords[i]);        
-           adminAreasMap[adminArea] = UtilitiesService.getUrlDomain(allOnlineResources[0].url);
-               
-           
-       }
+        // Set up a map of admin areas + URL's that belong to each
+        var adminAreasMap = {};
+        for (var i = 0; i < cswRecords.length; i++) {
+            var adminArea = cswRecords[i]['adminArea'];
+            var allOnlineResources = LayerManagerService.getOnlineResourcesFromCSW(cswRecords[i]);        
+            adminAreasMap[adminArea] = UtilitiesService.getUrlDomain(allOnlineResources[0].url);   
+        }
 
-       //Set up a list of each unique admin area      
-       for(key in adminAreasMap){
-           $scope.providers.push({
-               label : key,
-               value : adminAreasMap[key]
-           });
-       }
+        // Set up a list of each unique admin area      
+        for(key in adminAreasMap){
+            $scope.providers.push({
+                label : key,
+                value : adminAreasMap[key]
+            });
+        }
        
-   };
+    };
    
    
    
@@ -58,7 +97,11 @@ allControllers.controller('loadFilterCtrl', ['$scope','$rootScope','$timeout','R
          RenderHandlerService.renderLayer(layer,param);
      };
      
-
+     /**
+      * Adds a new filter to be displayed in the panel
+      * @method addFilter
+      * @param filter filter object to be added to the panel
+      */
      $scope.addFilter = function(filter){
          if(filter==null){
              return;
@@ -78,10 +121,14 @@ allControllers.controller('loadFilterCtrl', ['$scope','$rootScope','$timeout','R
          
      };
      
+     /**
+      * Removes all filters displayed in panel
+      * @method clearFilter
+      */
      $scope.clearFilter = function(){
          $scope.optionalFilters=[];
-         $scope.select.filter={};
      };
+     
      
      $scope.getKey = function(options) {
          return Object.keys(options)[0];
