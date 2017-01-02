dc8247ad7ba5ff7e1372bc964d1086f3669e0711
Vincent Fazio <vincent.fazio@csiro.au>
2017-01-03 09:42:11 +1100

    AUS-2771 Convert plain <select> to a Bootstrap-style dropdown

--- src/main/webapp/mobile/js/assets/controllers/analytics/pressureDbCtrl.js
+++ src/main/webapp/mobile/js/assets/controllers/analytics/pressureDbCtrl.js
@@ -6,26 +6,42 @@
  */
 allControllers.controller('pressureDbCtrl', ['$scope','GoogleMapService','$http','LayerManagerService','UtilitiesService','$timeout','$filter','RenderHandlerService','RenderHandlerService',
                                                     function ($scope,GoogleMapService,$http,LayerManagerService,UtilitiesService,$timeout,$filter,RenderHandlerService,RenderHandlerService) {
+    
+    /* Value displayed in property selector */
+    $scope.propertySelectLabel = "-- Select Property --";
+    
     $scope.broadcast = function() {
         $timeout(function() {
-          $scope.$broadcast('reCalcViewDimensions');
+            $scope.$broadcast('reCalcViewDimensions');
         });
-      }; 
+    }; 
     
-       $scope.param = {
-               ccLevels : 9            
-       };
+    $scope.param = {
+        ccLevels : 9            
+    };
     
-       $scope.slider = {          
-            options: {
-              floor: 3,
-              ceil: 9,
-              step: 1             
-            }
-        };
+    $scope.slider = {          
+        options: {
+            floor: 3,
+            ceil: 9,
+            step: 1             
+        }
+    };
        
-       $scope.addLayer = function(layer){
-           RenderHandlerService.renderLayer(layer,$scope.param);
-       };
+    $scope.addLayer = function(layer){
+        RenderHandlerService.renderLayer(layer,$scope.param);
+    };
+    
+    /**
+     * Sets the value displayed in the property dropdown selector and updates '$scope.param'
+     * to trigger the slider below
+     * @method setPropSelectList
+     * @param val value to be set in dropdown selector
+     */
+    $scope.setPropSelectList = function(val) {
+        var setVal = val;
+        $scope.propertySelectLabel = setVal;
+        $scope.param.ccProperty=setVal;
+    }
     
 }]);
