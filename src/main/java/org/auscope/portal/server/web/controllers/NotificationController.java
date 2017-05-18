186d336fcf6777bc6dc5a4e5a2cb5dd64306e287
joshvote <Josh.Vote@csiro.au>
2017-05-18 16:47:58 +0800

    AUS-2822 Backend for accessing recent notifications via twitter

---
+++ src/main/java/org/auscope/portal/server/web/controllers/NotificationController.java
@@ -0,0 +1,55 @@
+package org.auscope.portal.server.web.controllers;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.auscope.portal.core.server.controllers.BasePortalController;
+import org.auscope.portal.server.web.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Provides a controller interface into some basic administration functionality/tests
+ *
+ * @author Josh Vote
+ *
+ */
+@Controller
+public class NotificationController extends BasePortalController {
+
+    private final Log log = LogFactory.getLog(getClass());
+
+    private NotificationService notificationService;
+
+    /**
+     * Creates a new instance of this class
+     */
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    /**
+     * Gets the set of (cached) notifications for this service
+     *
+     * @return
+     * @throws URISyntaxException
+     */
+    @RequestMapping("/getNotifications.do")
+    public ModelAndView getNotifications() throws URISyntaxException {
+
+        try {
+            List<NotificationService.Notification> ns = notificationService.getRecentNotifications();
+
+            return generateJSONResponseMAV(true, ns, "");
+        } catch (Exception ex) {
+            log.debug("Unable to get notifications: " + ex.getMessage());
+            log.error("Error:", ex);
+            return generateJSONResponseMAV(false);
+        }
+    }
+}
---
+++ src/main/java/org/auscope/portal/server/web/service/NotificationService.java
@@ -0,0 +1,159 @@
+package org.auscope.portal.server.web.service;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.auscope.portal.core.services.PortalServiceException;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Service class for pulling simple notifications from Twitter
+ * @author Josh Vote (CSIRO)
+ *
+ */
+public class NotificationService {
+    private final Log log = LogFactory.getLog(getClass());
+
+    private Twitter twitter;
+    private String notificationAccount;
+    private List<Notification> cache;
+    private long cachePopulated;
+    private long cacheMaxAge = 15 * 60 * 1000; // 15 minutes
+    private int maxTweetAgeDays = 14; //How many days in the past to look back for notifications
+
+    public NotificationService(String notificationAccount, String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+            .setOAuthConsumerKey(consumerKey)
+            .setOAuthConsumerSecret(consumerSecret)
+            .setOAuthAccessToken(accessToken)
+            .setOAuthAccessTokenSecret(accessTokenSecret);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter = tf.getInstance();
+        this.notificationAccount = notificationAccount;
+        this.cache = null;
+        this.cachePopulated = 0L;
+    }
+
+    /**
+     * How long the cache remains valid in milliseconds
+     * @return
+     */
+    public long getCacheMaxAge() {
+        return cacheMaxAge;
+    }
+
+    /**
+     * How long the cache remains valid in milliseconds
+     * @param cacheMaxAge
+     */
+    public synchronized void setCacheMaxAge(long cacheMaxAge) {
+        this.cacheMaxAge = cacheMaxAge;
+    }
+
+    /**
+     * How old a tweet can be in days before it's ignored as a notification
+     * @return
+     */
+    public int getMaxTweetAgeDays() {
+        return maxTweetAgeDays;
+    }
+
+    /**
+     * How old a tweet can be in days before it's ignored as a notification
+     * @param maxTweetAgeDays
+     */
+    public synchronized void setMaxTweetAgeDays(int maxTweetAgeDays) {
+        this.maxTweetAgeDays = maxTweetAgeDays;
+    }
+
+    /**
+     * Gets the cached set of notifications (if valid) otherwise makes a synchronous
+     * request to Twitter to update the cache and then returns that value
+     *
+     * Only grabs notifications from the past maxTweetAgeDays days
+     *
+     * @return
+     */
+    public synchronized List<Notification> getRecentNotifications() throws PortalServiceException {
+
+        //If cache is valid, use that
+        Calendar calendar = Calendar.getInstance();
+        if (cache != null &&
+            (calendar.getTimeInMillis() - cachePopulated) < cacheMaxAge) {
+            return cache;
+        }
+
+        try {
+            //Fire a query for tweets from the account made in the last X days
+            Query query = new Query("from:" + notificationAccount);
+
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+            query.since(LocalDate.now().minusDays(maxTweetAgeDays).format(format));
+
+            QueryResult result = twitter.search(query);
+            cache = result.getTweets()
+                .stream()
+                .map(status -> new Notification(status.getText(), status.getUser().getScreenName(), status.getCreatedAt()))
+                .collect(Collectors.toList());
+            cachePopulated = Calendar.getInstance().getTimeInMillis();
+            return cache;
+        } catch (TwitterException ex) {
+            throw new PortalServiceException("Exception accessing notifications for " + notificationAccount + " : " + ex.getMessage(), ex);
+        }
+
+    }
+
+    /**
+     * Simple representation of a notification
+     * @author Josh Vote (CSIRO)
+     *
+     */
+    public static class Notification implements Serializable {
+        private static final long serialVersionUID = -787217258187312614L;
+        private String content;
+        private String author;
+        private Date time;
+
+        public Notification() {
+            super();
+        }
+        public Notification(String content, String author, Date time) {
+            super();
+            this.content = content;
+            this.author = author;
+            this.time = time;
+        }
+        public String getContent() {
+            return content;
+        }
+        public void setContent(String content) {
+            this.content = content;
+        }
+        public String getAuthor() {
+            return author;
+        }
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+        public Date getTime() {
+            return time;
+        }
+        public void setTime(Date time) {
+            this.time = time;
+        }
+    }
+}
