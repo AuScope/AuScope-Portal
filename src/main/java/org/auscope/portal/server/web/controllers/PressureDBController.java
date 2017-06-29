67c6c62c2f1a36cd2f5af0b59ab9655bc53d8bcf
Lingbo Jiang <Lingbo.Jiang@csiro.au>
2017-06-29 11:10:57 +1000

    AUS-2856 Upgraded PressureDB to geosciml-portrayal-4.0.

--- src/main/java/org/auscope/portal/server/web/controllers/PressureDBController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/PressureDBController.java
@@ -240,7 +240,7 @@ public class PressureDBController extends BasePortalController {
             filter = pressureDBFilter.getFilterString(null);
         }
         String style = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-                + "<StyledLayerDescriptor version=\"1.0.0\" xmlns:gsmlp=\"http://xmlns.geosciml.org/geosciml-portrayal/2.0\" "
+                + "<StyledLayerDescriptor version=\"1.0.0\" xmlns:gsmlp=\"http://xmlns.geosciml.org/geosciml-portrayal/4.0\" "
                 + "xsi:schemaLocation=\"http://www.opengis.net/sld StyledLayerDescriptor.xsd\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:gsml=\"urn:cgi:xmlns:CGI:GeoSciML:2.0\" xmlns:sld=\"http://www.opengis.net/sld\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
                 + "<NamedLayer>"
                 + "<Name>"
@@ -293,7 +293,7 @@ public class PressureDBController extends BasePortalController {
             styleRules += getStyleRuleByIndex(isLegend,3,boreholeName,custodian,dateOfDrillingStart,dateOfDrillingEnd,ccProperty);
             styleRules += getStyleRuleByIndex(isLegend,4,boreholeName,custodian,dateOfDrillingStart,dateOfDrillingEnd,ccProperty);
         String style = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-                + "<StyledLayerDescriptor version=\"1.0.0\" xmlns:gsmlp=\"http://xmlns.geosciml.org/geosciml-portrayal/2.0\" "
+                + "<StyledLayerDescriptor version=\"1.0.0\" xmlns:gsmlp=\"http://xmlns.geosciml.org/geosciml-portrayal/4.0\" "
                 + "xsi:schemaLocation=\"http://www.opengis.net/sld StyledLayerDescriptor.xsd\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:gsml=\"urn:cgi:xmlns:CGI:GeoSciML:2.0\" xmlns:sld=\"http://www.opengis.net/sld\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
                 + "<NamedLayer>"
                 + "<Name>"
