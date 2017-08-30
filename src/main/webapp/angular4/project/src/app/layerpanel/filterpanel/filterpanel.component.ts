e150d6c417a11f3c7edab226029673f6b8b9727e
Victor Tey <victor.tey@csiro.au>
2017-08-30 15:16:18 +0800

    AUS-2897:filtering for rendering checkbox model uses string. converting  to boolean

--- src/main/webapp/angular4/project/src/app/layerpanel/filterpanel/filterpanel.component.ts
+++ src/main/webapp/angular4/project/src/app/layerpanel/filterpanel/filterpanel.component.ts
@@ -1,18 +1,20 @@
-import { LayerModel } from '../../portal-core-ui/model/data/layer.model';
-import { LayerHandlerService } from '../../portal-core-ui/service/cswrecords/layer-handler.service';
-import { FilterPanelService } from '../../portal-core-ui/service/filterpanel/filterpanel-service';
-import { OlMapService } from '../../portal-core-ui/service/openlayermap/ol-map.service';
-import { UtilitiesService } from '../../portal-core-ui/utility/utilities.service';
-import { Component, Input} from '@angular/core';
+import {LayerModel} from '../../portal-core-ui/model/data/layer.model';
+import {LayerHandlerService} from '../../portal-core-ui/service/cswrecords/layer-handler.service';
+import {FilterPanelService} from '../../portal-core-ui/service/filterpanel/filterpanel-service';
+import {OlMapService} from '../../portal-core-ui/service/openlayermap/ol-map.service';
+import {UtilitiesService} from '../../portal-core-ui/utility/utilities.service';
+import {Component, Input, OnInit} from '@angular/core';
 import * as _ from 'lodash';
 
 @Component({
-    selector: 'app-filter-panel',
-    templateUrl: './filterpanel.component.html'
+  selector: 'app-filter-panel',
+  templateUrl: './filterpanel.component.html'
 })
 
 
-export class FilterPanelComponent {
+export class FilterPanelComponent implements OnInit {
+
+
 
 
   @Input() layer: LayerModel;
@@ -24,6 +26,18 @@ export class FilterPanelComponent {
     private filterPanelService: FilterPanelService) {
     this.providers = [];
     this.optionalFilters = [];
+  }
+
+  ngOnInit(): void {
+    if (this.layer.filterCollection && this.layer.filterCollection['mandatoryFilters']) {
+      const mandatoryFilters = this.layer.filterCollection['mandatoryFilters'];
+
+      for (const mandatoryFilter of mandatoryFilters) {
+        if (mandatoryFilter['type'] === 'MANDATORY.CHECKBOX') {
+          mandatoryFilter['value'] = (mandatoryFilter['type'] === 'true')
+        }
+      }
+    }
   }
 
   public addLayer(layer): void {
@@ -49,71 +63,71 @@ export class FilterPanelComponent {
 
 
 
-   /**
-     * Adds a new filter to be displayed in the panel
-     * @method addFilter
-     * @param filter filter object to be added to the panel
-     * @param addEmpty if true, set filter value to be empty.
-     */
-    public addFilter(filter, addEmpty?: boolean): void {
-        if (filter == null) {
-            return;
-        }
-        for (const filterobject of this.optionalFilters) {
-          if (filterobject['label'] === filter['label']) {
-            return;
-          }
-        }
-        if (UtilitiesService.isEmpty(this.providers) && filter.type === 'OPTIONAL.PROVIDER') {
-            this.getProvider();
-            filter.value = {};
-            for (const provider of this.providers) {
-              filter.value[provider['value']] = true;
-            }
+  /**
+    * Adds a new filter to be displayed in the panel
+    * @method addFilter
+    * @param filter filter object to be added to the panel
+    * @param addEmpty if true, set filter value to be empty.
+    */
+  public addFilter(filter, addEmpty?: boolean): void {
+    if (filter == null) {
+      return;
+    }
+    for (const filterobject of this.optionalFilters) {
+      if (filterobject['label'] === filter['label']) {
+        return;
+      }
+    }
+    if (UtilitiesService.isEmpty(this.providers) && filter.type === 'OPTIONAL.PROVIDER') {
+      this.getProvider();
+      filter.value = {};
+      for (const provider of this.providers) {
+        filter.value[provider['value']] = true;
+      }
 
 
-        }
-        if (UtilitiesService.isEmpty(filter.options) && filter.type === 'OPTIONAL.DROPDOWNREMOTE') {
-            this.filterPanelService.getFilterRemoteParam(filter.url).subscribe(response => {
-                filter.options = response;
-                this.optionalFilters.push(filter);
-            });
-            return;
-        }
+    }
+    if (UtilitiesService.isEmpty(filter.options) && filter.type === 'OPTIONAL.DROPDOWNREMOTE') {
+      this.filterPanelService.getFilterRemoteParam(filter.url).subscribe(response => {
+        filter.options = response;
         this.optionalFilters.push(filter);
+      });
+      return;
+    }
+    this.optionalFilters.push(filter);
 
-    };
+  };
 
   /**
      * Assembles a list of providers, which will be displayed in the panel
      * @method getProvider
      */
-    private getProvider(): void {
-        const cswRecords = this.layer.cswRecords;
+  private getProvider(): void {
+    const cswRecords = this.layer.cswRecords;
 
-        // Set up a map of admin areas + URL's that belong to each
-        const adminAreasMap = {};
-        for (let i = 0; i < cswRecords.length; i++) {
-            const adminArea = cswRecords[i].adminArea;
-            const allOnlineResources = this.layerHandlerService.getOnlineResourcesFromCSW(cswRecords[i]);
-            adminAreasMap[adminArea] = UtilitiesService.getUrlDomain(allOnlineResources[0].url);
-        }
-
-        // Set up a list of each unique admin area
-        for (const key in adminAreasMap) {
-           this.providers.push({
-                label : key,
-                value : adminAreasMap[key]
-            });
-        }
-
-    };
-
-    /**
-     * refresh and clear the filters;
-     */
-    public refreshFilter(): void {
-      this.optionalFilters = [];
-      this.selectedFilter = {};
+    // Set up a map of admin areas + URL's that belong to each
+    const adminAreasMap = {};
+    for (let i = 0; i < cswRecords.length; i++) {
+      const adminArea = cswRecords[i].adminArea;
+      const allOnlineResources = this.layerHandlerService.getOnlineResourcesFromCSW(cswRecords[i]);
+      adminAreasMap[adminArea] = UtilitiesService.getUrlDomain(allOnlineResources[0].url);
     }
+
+    // Set up a list of each unique admin area
+    for (const key in adminAreasMap) {
+      this.providers.push({
+        label: key,
+        value: adminAreasMap[key]
+      });
+    }
+
+  };
+
+  /**
+   * refresh and clear the filters;
+   */
+  public refreshFilter(): void {
+    this.optionalFilters = [];
+    this.selectedFilter = {};
+  }
 }
