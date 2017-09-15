d82e7e8f0aec0b5d3277854bdf386f41cc623b9d
Victor Tey <victor.tey@csiro.au>
2017-09-15 14:16:17 +0800

    AUS-2914: Angular 4 port download interface: tidy up the UI

--- src/main/webapp/angular4/project/src/app/layerpanel/downloadpanel/downloadpanel.component.ts
+++ src/main/webapp/angular4/project/src/app/layerpanel/downloadpanel/downloadpanel.component.ts
@@ -1,11 +1,12 @@
-import { Bbox } from '../../portal-core-ui/model/data/bbox.model';
+import {Bbox} from '../../portal-core-ui/model/data/bbox.model';
 import {LayerModel} from '../../portal-core-ui/model/data/layer.model';
 import {LayerHandlerService} from '../../portal-core-ui/service/cswrecords/layer-handler.service';
-import { OlMapService } from '../../portal-core-ui/service/openlayermap/ol-map.service';
-import { DownloadWfsService } from '../../portal-core-ui/service/wfs/download/download-wfs.service';
-import {Component, Input} from '@angular/core';
+import {OlMapService} from '../../portal-core-ui/service/openlayermap/ol-map.service';
+import {DownloadWfsService} from '../../portal-core-ui/service/wfs/download/download-wfs.service';
+import {Component, Input, OnInit} from '@angular/core';
 import {UtilitiesService} from '../../portal-core-ui/utility/utilities.service';
-import { saveAs } from 'file-saver/FileSaver';
+import {saveAs} from 'file-saver/FileSaver';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-download-panel',
@@ -13,17 +14,35 @@ import { saveAs } from 'file-saver/FileSaver';
 })
 
 
-export class DownloadPanelComponent {
+export class DownloadPanelComponent implements OnInit {
+
 
   @Input() layer: LayerModel;
   bbox: Bbox;
+  drawStarted: boolean;
+  downloadStarted: boolean;
+  isCsvSupportedLayer: boolean;
 
   constructor(private layerHandlerService: LayerHandlerService, private olMapService: OlMapService, private downloadWfsService: DownloadWfsService) {
     this.bbox = null;
+    this.drawStarted = false;
+    this.downloadStarted = false;
+
+  }
+
+  ngOnInit(): void {
+    if (this.layer) {
+      this.isCsvSupportedLayer = environment.csvSupportedLayer.indexOf(this.layer.id) >= 0;
+    } else {
+      this.isCsvSupportedLayer = false;
+    }
   }
 
   public drawBound(): void {
+    setTimeout(() => this.drawStarted = true, 0);
+
     this.olMapService.drawBound().subscribe((vector) => {
+      this.drawStarted = false;
       const features = vector.getSource().getFeatures();
       const me = this;
       // Go through this array and get coordinates of their geometry.
@@ -40,14 +59,18 @@ export class DownloadPanelComponent {
     });
   }
 
+
+
   public clearBound(): void {
     this.bbox = null;
   }
 
   public download(): void {
+    this.downloadStarted = true;
     this.downloadWfsService.download(this.layer, this.bbox).subscribe(value => {
-       const blob = new Blob([value], {type: 'application/zip'});
-        saveAs(blob, 'download.zip');
+      this.downloadStarted = false;
+      const blob = new Blob([value], {type: 'application/zip'});
+      saveAs(blob, 'download.zip');
     });
   }
 }
