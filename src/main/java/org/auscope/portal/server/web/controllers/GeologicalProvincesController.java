787d2beb2c30d229889818c7815ea2ed1633316b
Peter Warren <peter.warren@csiro.au>
2018-07-03 17:15:30 +1000

    Created multi-coloured province style

--- src/main/java/org/auscope/portal/server/web/controllers/GeologicalProvincesController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/GeologicalProvincesController.java
@@ -19,9 +19,9 @@ public class GeologicalProvincesController extends BasePortalController {
     private GeologicalProvincesService geologicalProvincesService;
     @Autowired
     public GeologicalProvincesController(GeologicalProvincesService geologicalProvincesService) {
-    	this.geologicalProvincesService = geologicalProvincesService;
-    	//this.geologicalProvincesService = new GeologicalProvincesService();
-    	
+        this.geologicalProvincesService = geologicalProvincesService;
+        //this.geologicalProvincesService = new GeologicalProvincesService();
+
     }
 
     /**
@@ -38,11 +38,11 @@ public class GeologicalProvincesController extends BasePortalController {
             @RequestParam(required = false, value = "name") String name,
             @RequestParam(required = false, value = "optionalFilters") String optionalFilters,
             HttpServletResponse response) throws Exception {
-    	
-        response.setContentType("text/xml");    	
+        
+        response.setContentType("text/xml");
         String style = "";        
         String filter = this.geologicalProvincesService.getGeologicalProvincesFilter(name,optionalFilters);
-        style = getStyle("gml:ProvinceFullExtent","#ff0000", name, filter);
+        style = getStyle("gml:ProvinceFullExtent", name, filter);
         ByteArrayInputStream styleStream = new ByteArrayInputStream(style.getBytes());
         OutputStream outputStream = response.getOutputStream();
 
@@ -52,7 +52,7 @@ public class GeologicalProvincesController extends BasePortalController {
         outputStream.close();
     }
 
-    public String getStyle(String layerName, String color, String name, String filter) {
+    public String getStyle(String layerName, String name, String filter) {
         String style = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                 + "<StyledLayerDescriptor version=\"1.0.0\" xmlns:gsmlp=\"http://xmlns.geosciml.org/geosciml-portrayal/4.0\" "
                 + "xsi:schemaLocation=\"http://www.opengis.net/sld StyledLayerDescriptor.xsd\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:gsml=\"urn:cgi:xmlns:CGI:GeoSciML:2.0\" xmlns:sld=\"http://www.opengis.net/sld\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
@@ -68,14 +68,43 @@ public class GeologicalProvincesController extends BasePortalController {
                 + "<Rule>"
                 + "<Name>Boreholes</Name>"
                 + filter
-                + "<PolygonSymbolizer>"
-                + "<Stroke>"
-                + "<CssParameter name=\"stroke\">"
-                + color 
-                + "</CssParameter>"
-                + "<CssParameter name=\"stroke-width\">0.1</CssParameter>"
-                + "</Stroke>"
-                + "</PolygonSymbolizer>" 
+                + "<sld:PolygonSymbolizer>"
+                + "<sld:Fill>"
+                + "<sld:CssParameter name=\"fill\">"
+                + "<ogc:Function name=\"Recode\">"
+                + "<ogc:Function name=\"IEEERemainder\">"
+                + "<ogc:PropertyName>OBJECTID</ogc:PropertyName>"
+                + "<ogc:Function name=\"parseInt\">"
+                + "<ogc:Literal>9</ogc:Literal>"
+                + "</ogc:Function>"
+                + "</ogc:Function>"
+                + "<ogc:Literal>-4</ogc:Literal>"
+                + "<ogc:Literal>#8dd3c7</ogc:Literal>"
+                + "<ogc:Literal>-3</ogc:Literal>"
+                + "<ogc:Literal>#ffffb3</ogc:Literal>"
+                + "<ogc:Literal>-2</ogc:Literal>"
+                + "<ogc:Literal>#bebada</ogc:Literal>"
+                + "<ogc:Literal>-1</ogc:Literal>"
+                + "<ogc:Literal>#fb8072</ogc:Literal>"
+                + "<ogc:Literal>0</ogc:Literal>"
+                + "<ogc:Literal>#80b1d3</ogc:Literal>"
+                + "<ogc:Literal>1</ogc:Literal>"
+                + "<ogc:Literal>#fdb462</ogc:Literal>"
+                + "<ogc:Literal>2</ogc:Literal>"
+                + "<ogc:Literal>#b3de69</ogc:Literal>"
+                + "<ogc:Literal>3</ogc:Literal>"
+                + "<ogc:Literal>#fccde5</ogc:Literal>"
+                + "<ogc:Literal>4</ogc:Literal>"
+                + "<ogc:Literal>#d9d9d9</ogc:Literal>"
+                + "</ogc:Function>"
+                + "</sld:CssParameter>"
+                + "<CssParameter name=\"fill-opacity\">0.4</CssParameter>"
+                + "</sld:Fill>"
+                + "<sld:Stroke>"
+                + "<sld:CssParameter name=\"stroke\">#000000</sld:CssParameter>"
+                + "<sld:CssParameter name=\"stroke-width\">1</sld:CssParameter>"
+                + "</sld:Stroke>"
+                + "</sld:PolygonSymbolizer>" 
                 + "</Rule>"
                 + "</FeatureTypeStyle>"
                 + "</UserStyle>" + "</NamedLayer>" + "</StyledLayerDescriptor>";      
